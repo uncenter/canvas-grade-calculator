2f4d1e5e23eb0e1f6ab0669659ac4aa202540e4b
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-26 21:15:33 -0400

    feat: inital commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Canvas Grade Calculator
+
+Calculate grade totals for Canvas courses that have it disabled.
+
+## Usage
+
+### Violentmonkey
+
+- [Install Violentmonkey](https://violentmonkey.github.io/get-it/).
+  - [Chrome](https://chrome.google.com/webstore/detail/violentmonkey/jinjaccalgkegednnccohejagnlnfdag)/[Firefox](https://addons.mozilla.org/en-US/firefox/addon/violentmonkey/)/[Microsoft Edge](https://microsoftedge.microsoft.com/addons/detail/violentmonkey/eeagobfjdenkkddmbclomhiblgggliao)
+- Add this userscript to Violentmonkey:
+  - Open the Violentmonkey extension by clicking on the extension icon.
+  - Open the "dashboard" by clicking the gear/settings icon.
+  - Press the "+" button in the top left of the dashboard and click "Install from URL".
+  - Paste the following URL into the field that pops up:
+
+```
+https://gist.github.com/uncenter/f3f0dc2faa50a4edcaf2c8b4521fdc67/raw
+```
+
+### Manual
+
+You can also just copy the contents of the script and paste it directly into the browser console.
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name        Canvas Grade Calculator
+// @description Calculate grade totals for Canvas courses that have it disabled.
+// @namespace   https://github.com/uncenter/canvas-grade-calculator
+// @match       https://*.instructure.com/courses/*/grades
+// @grant       none
+// @downloadURL https://github.com/uncenter/canvas-grade-calculator/raw/main/index.js
+// @homepageURL https://github.com/uncenter/canvas-grade-calculator
+// @version     0.1.0
+// @author      uncenter
+// @license     MIT
+// ==/UserScript==
+
+function run() {
+	if (!document.querySelector(".ic-app-main-content")) {
+		console.error("Not on Canvas!");
+		return;
+	}
+
+	if (!document.querySelector("#grade-summary-content")) {
+		console.error("Not on grades page!");
+		return;
+	}
+
+	const weights = {};
+
+	document
+		.querySelectorAll('[aria-label="Assignment Weights"] > table tbody > tr')
+		.forEach((e) => {
+			const group = e.querySelector("th").textContent;
+			if (group === "Total") return;
+			const weight =
+				parseFloat(e.querySelector("td").textContent.slice(0, 2)) / 100;
+			weights[group] = weight;
+		});
+
+	const assignments = [];
+
+	document
+		.querySelectorAll("#grades_summary tr.assignment_graded.student_assignment")
+		.forEach((e) => {
+			let earned, available, title, group;
+
+			const a = e.querySelector("th.title");
+			title = a.querySelector("a").textContent;
+			group = a.querySelector("div.context").textContent;
+
+			const grades = e.querySelector(
+				"td.assignment_score > div > span.tooltip > span.grade"
+			);
+			for (let i = 0; i < grades.childNodes.length; i++) {
+				const node = grades.childNodes[i];
+				if (
+					node.nodeType === Node.TEXT_NODE &&
+					node.textContent.trim() !== ""
+				) {
+					earned = parseFloat(node.textContent.trim());
+					break;
+				}
+			}
+			available = parseFloat(
+				grades.nextElementSibling.textContent.replace("/", "").trim()
+			);
+
+			assignments.push({ earned, available, title, group });
+		});
+
+	if (assignments.length === 0) {
+		console.warn("No graded assignments found!");
+		return;
+	}
+
+	const totalsPerGroup = assignments.reduce(
+		(acc, { group, earned, available }) => {
+			acc[group] = acc[group] || { totalEarned: 0, totalAvailable: 0 };
+			acc[group].totalEarned += earned;
+			acc[group].totalAvailable += available;
+			return acc;
+		},
+		{}
+	);
+
+	const weightedPerGroup = {};
+	for (const category in totalsPerGroup) {
+		const { totalEarned, totalAvailable } = totalsPerGroup[category];
+		weightedPerGroup[category] = (totalEarned / totalAvailable) * 100 || null;
+	}
+
+	let grade = 0;
+	if (Object.entries(weights).length !== 0) {
+		for (const category in weights) {
+			grade += weightedPerGroup[category] * weights[category];
+		}
+	} else {
+		console.log("Categories are not weighted.");
+		const scores = Object.values(weightedPerGroup).filter((x) => x !== null);
+		grade = scores.reduce((total, score) => total + score, 0) / scores.length;
+	}
+
+	console.log(
+		`${grade.toFixed(2)}% across ${assignments.length} graded assignments.`
+	);
+
+	document.querySelector(
+		"#student-grades-final"
+	).outerHTML = `<div class="student_assignment final_grade">Total: <span class="grade">${grade.toFixed(
+		2
+	)}%</span></div>`;
+}
+
+run();
